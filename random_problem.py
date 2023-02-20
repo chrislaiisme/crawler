9f0f6279ea1199365e86c68cfe0927d93b0c6844
chrislaiisme <95356860+chrislaiisme@users.noreply.github.com>
2023-02-20 21:23:54 +0800

    Create random_problem.py

---
+++ random_problem.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+import webbrowser
+
+Id = input("Please input your TIOJ id: \n")
+
+r = requests.get("https://tioj.ck.tp.edu.tw/users/" + Id)
+# print(r.text)
+
+soup = BeautifulSoup(r.text,"html.parser")
+sel = soup.select("div.col-md-8 a")
+
+problems = []
+for i in range(0, 3):
+    problems.append([])
+
+for i in sel:
+    s = i["class"][0]
+    s2 = i.text
+
+    if s == "text-success":
+        problems[0].append(s2)
+    elif s == "text-warning":
+        problems[1].append(s2)
+    else:
+        problems[2].append(s2)
+
+while True:
+    op = int(input("Get a random problem from problems that you've:\n\t1. AC ed, 2. WA ed, 3. None touched")) - 1
+    pos = int(random.randint(0, len(problems[op]) - 1))
+    webbrowser.open_new_tab("https://tioj.ck.tp.edu.tw/problems/" + problems[op][pos])
+    
