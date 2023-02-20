a41901da1d80a0545f51b2093705f36b6f358362
chrislaiisme <95356860+chrislaiisme@users.noreply.github.com>
2023-02-20 14:37:35 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,27 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://tioj.ck.tp.edu.tw/users/chrislaiisme")
-# print(r.text)
-
-soup = BeautifulSoup(r.text,"html.parser")
-sel = soup.select("div.col-md-8 a")
-
-success = []
-muted = []
-warning = []
-
-for i in sel:
-    s = i["class"][0]
-    s2 = i.text
+while True:
+    Id = input("Please input your TIOJ id: \n")
     
-    if s == "text-success":
-        success.append(s2)
-    elif s == "text-warning":
-        warning.append(s2)
-    else:
-        muted.append(s2)
+    r = requests.get("https://tioj.ck.tp.edu.tw/users/" + Id)
+    # print(r.text)
+    
+    soup = BeautifulSoup(r.text,"html.parser")
+    sel = soup.select("div.col-md-8 a")
 
-print("Success:")
-for i in success:
-    print(i)
+    success = []
+    muted = []
+    warning = []
+
+    for i in sel:
+        s = i["class"][0]
+        s2 = i.text
+
+        if s == "text-success":
+            success.append(s2)
+        elif s == "text-warning":
+            warning.append(s2)
+        else:
+            muted.append(s2)
+
+    print("AC:")
+    for i in success:
+        print(i)
+
+    '''
+    print()
+    print("WA:")
+    for i in warning:
+        print(i)
+    print()
+    print("None:")
+    for i in muted:
+        print(i)
+    '''
