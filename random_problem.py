8c820b3efca10a700aa934fba6b089a9df6c77bc
chrislaiisme <95356860+chrislaiisme@users.noreply.github.com>
2023-02-21 21:55:14 +0800

    Update random_problem.py

--- random_problem.py
+++ random_problem.py
@@ -3,31 +3,36 @@ from bs4 import BeautifulSoup
 import random
 import webbrowser
 
-Id = input("Please input your TIOJ id: \n")
+while True: 
+    Id = input("Please input your TIOJ id: \n")
 
-r = requests.get("https://tioj.ck.tp.edu.tw/users/" + Id)
-# print(r.text)
+    r = requests.get("https://tioj.ck.tp.edu.tw/users/" + Id)
+    # print(r.text)
 
-soup = BeautifulSoup(r.text,"html.parser")
-sel = soup.select("div.col-md-8 a")
+    soup = BeautifulSoup(r.text,"html.parser")
+    sel = soup.select("div.col-md-8 a")
 
-problems = []
-for i in range(0, 3):
-    problems.append([])
-
-for i in sel:
-    s = i["class"][0]
-    s2 = i.text
-
-    if s == "text-success":
-        problems[0].append(s2)
-    elif s == "text-warning":
-        problems[1].append(s2)
+    if len(sel) == 0:
+        print("Id doesn't exist")
     else:
-        problems[2].append(s2)
+        break;
 
 while True:
     op = int(input("Get a random problem from problems that you've:\n\t1. AC ed, 2. WA ed, 3. None touched")) - 1
+    problems = []
+    for i in range(0, 3):
+        problems.append([])
+
+    for i in sel:
+        s = i["class"][0]
+        s2 = i.text
+
+        if s == "text-success":
+            problems[0].append(s2)
+        elif s == "text-warning":
+            problems[1].append(s2)
+        else:
+            problems[2].append(s2)
+            
     pos = int(random.randint(0, len(problems[op]) - 1))
     webbrowser.open_new_tab("https://tioj.ck.tp.edu.tw/problems/" + problems[op][pos])
-    
