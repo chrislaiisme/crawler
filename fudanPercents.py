d174b0e254fcfb055a48bc6e092e3479cd1a663f
chrislaiisme <95356860+chrislaiisme@users.noreply.github.com>
2023-06-12 13:43:29 +0800

    Update fudanPercents.py

--- fudanPercents.py
+++ fudanPercents.py
@@ -10,7 +10,7 @@ for i in range(572, 0, -1):
     goal = soup.find(string="簡單") # find "簡單"
     goal = goal.parent.parent.parent.parent # go back to parent
     goal = goal.find_all("td")[1].find("font") # find real goal text
-    if goal.text.find("放榜") != -1:
+    if goal.text.find("大學") != -1:
         print(str(i) + ":\n" + goal.text + "\n==================================================\n")
 print("Finish")
 
