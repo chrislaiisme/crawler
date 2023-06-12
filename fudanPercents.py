08a7b2159d99467e8a6252f66b9e906c0890f8a8
chrislaiisme <95356860+chrislaiisme@users.noreply.github.com>
2023-06-12 14:28:54 +0800

    Update fudanPercents.py

--- fudanPercents.py
+++ fudanPercents.py
@@ -1,8 +1,9 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
 
-"""
 link = "https://www.fdhs.tyc.edu.tw/e-fdhs/news/out_news_detail_80.php?news_id="
+
+"""
 for i in range(573, 0, -1):
     res = req.get(link+str(i))
     res.encoding = 'big5'
@@ -15,5 +16,30 @@ for i in range(573, 0, -1):
 print("Finish")
 """
 
-arr = [558, 541, 523, 499, 467, 437, 406, 377, 346, 321, 278, 251, 204, 169]
-year= [111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100,  99,  98]
+arr = [558, 541, 523, 499, 467, 437, 406, 377, 346, 321, 278, 251, 204, 169] # id
+year= [111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100,  99,  98] # year
+chart = [] # chart
+schools = ["國立臺灣大學", "國立清華大學", "國立交通大學", "國立陽明交通大學", "國立成功大學", "國立政治大學"] # schools
+
+for i in range(0, len(arr)):
+    tmp = [year[i]] # the ith row in chart
+    res = req.get(link+str(arr[i]))
+    res.encoding = 'big5'
+    soup = bs(res.text, "html.parser")
+    s = soup.get_text();  
+    count = 0 # number of people
+    for j in range(0, len(schools)):
+        start = 0
+        end = len(s)
+        s2 = schools[j]
+        while True:
+            p = s.find(s2, start, end)
+            if p == -1:
+                break
+            count += 1
+            start = p + 1
+        if j == 0:
+            tmp.append(count)
+    tmp.append(count)
+    chart.append(tmp)
+print(chart)
