b84eec58863318732153dc36ea690594b8fed11c
chrislaiisme <95356860+chrislaiisme@users.noreply.github.com>
2023-06-19 14:21:59 +0800

    Update fudanPercents.py

--- fudanPercents.py
+++ fudanPercents.py
@@ -16,13 +16,15 @@ for i in range(573, 0, -1):
 print("Finish")
 """
 
-arr = [558, 541, 523, 499, 467, 437, 406, 377, 346, 321, 278, 251, 204, 169] # id
-year= [111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100,  99,  98] # year
+arr = [574, 558, 541, 523, 499, 467, 437, 406, 377, 346, 321, 278, 251, 204, 169] # id
+year= [112, 111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100,  99,  98] # year
+nop = [558,   0, 562,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0]
 chart = [] # chart
 schools = ["國立臺灣大學", "國立清華大學", "國立交通大學", "國立陽明交通大學", "國立成功大學", "國立政治大學"] # schools
+dft_nop = 565 # default nop
 
 for i in range(0, len(arr)):
-    tmp = [year[i]] # the ith row in chart
+    tmp = [year[i]] # year
     res = req.get(link+str(arr[i]))
     res.encoding = 'big5'
     soup = bs(res.text, "html.parser")
@@ -39,13 +41,42 @@ for i in range(0, len(arr)):
             count += 1
             start = p + 1
         if j == 0:
-            tmp.append(count)
-    tmp.append(count)
-    chart.append(tmp)
+            tmp.append(count) # NTU
+    tmp.append(count) # TFU
+    tmp.append('?' if nop[i] == 0 else nop[i]) # NOP
+    num = dft_nop if tmp[3] == '?' else tmp[3]
+    tmp.append(str(round(tmp[1]/num*100, 2)) + '%') # NTU%
+    tmp.append(str(round(tmp[2]/num*100, 2)) + '%') # TFU%
+    chart.append(tmp) # append row in chart
 
-s = "------|------|-------"
-print("  Year|   NTU|   TFU")
+s = ''
+for i in range(0, 6):
+    s += '-------'
+    if i != 5:
+        s += '|'
+print("   Year|    NTU|    TFU|    NOP|   NTU%|   TFU%")
 print(s)
 for i in chart:
-    for j in range(0, 3):
-        print('{:6}'.format(i[j]), end = ('\n' if j == 2 else '|'))
+    for j in range(0, 6):
+        print('{:>7}'.format(i[j]), end = ('\n' if j == 5 else '|'))
+print('Average:')
+NTUn = 0
+TFUn = 0
+NOPn = 0
+for i in chart:
+    NTUn += i[1]
+    TFUn += i[2]
+    NOPn += dft_nop if i[3] == '?' else i[3]
+print()
+print("NTU%: ", end = '')
+print(str(round(NTUn/NOPn*100, 2)) + '%')
+print("TFU%: ", end = '')
+print(str(round(TFUn/NOPn*100, 2)) + '%')
+print()
+print("*** Supplementary informations ***")
+print("NTU = National Taiwan University")
+print("TFU = Top Five Universities")
+print("NOP = Number Of People (565 by default if there's no data)")
+print()
+print("The number 565 comes from \"https://www.fdhs.tyc.edu.tw/campus/brief.htm\" with 3391/6 = 565.166666667")
+print("All data are from \"https://www.fdhs.tyc.edu.tw/\"")
